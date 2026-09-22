1a7c5767bb9108a8bc98703b37831e4763b596a1
Rinconi <tabletrinconi@gmail.com>
2026-09-22 02:34:59 +0200

    Update generate_report.py

--- generate_report.py
+++ generate_report.py
@@ -45,11 +45,15 @@ def generar():
     pdf.cell(0,6,"27-28/09/2026 - 15 partidos", ln=True, align='C')
     pdf.ln(4)
 
+    pdf.set_text_color(255, 255, 255) # Texto blanco
+    pdf.set_fill_color(40, 116, 166)   # Fondo azul
     pdf.set_font("Helvetica","B",10)
     pdf.cell(10,8,"N",1,0,'C')
-    pdf.cell(80,8,"LOCAL",1,0,'C')
+    pdf.cell(80, 8, "LOCAL", 1, 0, 'C', fill=True)
+    #pdf.cell(80,8,"LOCAL",1,0,'C')
     pdf.cell(80,8,"VISITANTE",1,1,'C')
 
+    
     pdf.set_font("Helvetica","",10)
     for i,(l,v) in enumerate(partidos,1):
         pdf.cell(10,8,str(i),1,0,'C')
